f67a6403e3fa5799e00ede7a074c203751cdadc1
Daniele Pestilli <dpestilli@funnelback.com>
2016-09-22 15:12:03 +0100

    Added another name to the spec for testing

--- spec/korean_name_spec.rb
+++ spec/korean_name_spec.rb
@@ -1,15 +1,19 @@
 describe KoreanName do
   context 'Korean name' do
     it '#to_en converts Korean to English' do
-      name = KoreanName::Translator.new :first_name => "연아", :last_name => "김"
+      name1 = KoreanName::Translator.new :first_name => "연아", :last_name => "김"
+      name2 = KoreanName::Translator.new :first_name => "연선", :last_name => "박"
 
-      expect(name.to_en).to eq({:first_name => "Yeonah", :last_name => "Kim"})
+      expect(name1.to_en).to eq({:first_name => "Yeonah", :last_name => "Kim"})
+      expect(name2.to_en).to eq({:first_name => "Yeonseon", :last_name => "Bak"})
     end
 
     it '#to_en leaves English in English' do
-      name = KoreanName::Translator.new(:first_name => "Yeonah", :last_name => "Kim")
+      name1 = KoreanName::Translator.new(:first_name => "Yeonah", :last_name => "Kim")
+      name2 = KoreanName::Translator.new(:first_name => "Yeonseon", :last_name => "Bak")
 
-      expect(name.to_en).to eq({first_name: "Yeonah", last_name: "Kim"})
+      expect(name1.to_en).to eq({first_name: "Yeonah", last_name: "Kim"})
+      expect(name2.to_en).to eq({:first_name => "Yeonseon", :last_name => "Bak"})
     end
   end
 
