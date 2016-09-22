e1510654ff292c97166e9ac6da4cdc86c6177341
Daniele Pestilli <dpestilli@funnelback.com>
2016-09-22 13:28:29 +0100

    Made dict an instance variable

--- lib/korean_name/translator.rb
+++ lib/korean_name/translator.rb
@@ -1,6 +1,8 @@
 module KoreanName
   class Translator
     def initialize(name)
+      @dict = YAML.load_file "lib/korean_name/dict.yml"
+
       if name =~ /\A\p{Hangul}+\Z/
         @kr_name = name
       else
@@ -12,11 +14,9 @@ module KoreanName
     end
 
     def to_en
-      dict = YAML.load_file "lib/korean_name/dict.yml"
       array = @kr_name.split ''
-
-      last_name = dict[array.shift]
-      first_name = array.map {|char| dict[char]}.compact.join('')
+      last_name = @dict[array.shift]
+      first_name = array.map {|char| @dict[char]}.compact.join('')
      
       last_name[0] = last_name[0].upcase
       first_name[0] = first_name[0].upcase
