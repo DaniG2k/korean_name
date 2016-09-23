7b9b57b52b59566c358c28a8061ab1b9bfc8a308
Daniele Pestilli <dpestilli@funnelback.com>
2016-09-23 10:18:24 +0100

    Adding #to_ko method

--- lib/korean_name/translator.rb
+++ lib/korean_name/translator.rb
@@ -4,21 +4,26 @@ module KoreanName
       @dict = YAML.load_file "lib/korean_name/dict.yml"
 
       if args[:first_name] =~ /\A\p{Hangul}+\Z/ && args[:last_name] =~ /\A\p{Hangul}+\Z/
-        @kr_first_name = args[:first_name]
-        @kr_last_name = args[:last_name]
+        @ko_first_name = args[:first_name]
+        @ko_last_name = args[:last_name]
       else
         @en_first_name = args[:first_name]
         @en_last_name = args[:last_name]
       end
     end
 
-    def to_kr
+    def to_ko
+      if @ko_first_name.present? && @ko_last_name.present?
+        {first_name: @ko_first_name, last_name: @ko_last_name}
+      else
+        {}
+      end
     end
 
     def to_en
-      if @kr_first_name.present? && @kr_last_name.present?
-        last_name = @dict[@kr_last_name]
-        first_name = @kr_first_name.split('').map {|char| @dict[char]}.compact.join('')
+      if @ko_first_name.present? && @ko_last_name.present?
+        last_name = @dict['ko'][@ko_last_name]
+        first_name = @ko_first_name.split('').map {|char| @dict['ko'][char]}.compact.join('')
 
         {first_name: first_name.titleize, last_name: last_name.titleize}
       else
--- spec/korean_name_spec.rb
+++ spec/korean_name_spec.rb
@@ -17,11 +17,21 @@ describe KoreanName do
     end
   end
 
-  #context 'English name' do
-  #  it 'converts to Korean' do
-  #    name = KoreanName.new "Kim Yeona"
+  context 'English name' do
+    it '#to_ko converts English to Korean' do
+      name1 = KoreanName::Translator.new :first_name => 'Yeonah', :last_name => 'Kim'
+      name2 = KoreanName::Translator.new :first_name => 'Yeonseon', :last_name => 'Bak'
 
-  #    expect(name.to_kr).to eq("김연아")
-  #  end
-  #end
+      expect(name1.to_ko).to eq({first_name: "연아", last_name: "김"})
+      expect(name2.to_ko).to eq({first_name: "연선", last_name: "박"})
+    end
+
+    it '#to_ko leaves Korean in Korean' do
+      name1 = KoreanName::Translator.new(:first_name => "연아", :last_name => "김")
+      name2 = KoreanName::Translator.new(:first_name => "연선", :last_name => "박")
+
+      expect(name1.to_ko).to eq({first_name: "연아", last_name: "김"})
+      expect(name2.to_ko).to eq({:first_name => "연선", :last_name => "박"})
+    end
+  end
 end
