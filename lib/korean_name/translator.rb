a4288970b0b97ff1ec8a00499edb8cc680ce7990
Daniele Pestilli <dpestilli@funnelback.com>
2016-09-23 10:57:44 +0100

    Finalized #to_en and #to_ko

--- lib/korean_name/translator.rb
+++ lib/korean_name/translator.rb
@@ -14,9 +14,25 @@ module KoreanName
 
     def to_ko
       if @ko_first_name.present? && @ko_last_name.present?
-        {first_name: @ko_first_name, last_name: @ko_last_name}
+        {
+          first_name: @ko_first_name,
+          last_name: @ko_last_name
+        }
       else
-        {}
+        @en_first_name.downcase!
+        @en_last_name.downcase!
+
+        part1_name = @en_first_name
+        part1_name = part1_name[0..-2] until @dict['en'][part1_name].present?
+        
+        part2_name = @en_first_name.gsub(part1_name, '')
+
+        first_name = [@dict['en'][part1_name], @dict['en'][part2_name]].join('').titleize
+
+        {
+          first_name: first_name,
+          last_name: @dict['en'][@en_last_name]
+        }
       end
     end
 
@@ -25,9 +41,15 @@ module KoreanName
         last_name = @dict['ko'][@ko_last_name]
         first_name = @ko_first_name.split('').map {|char| @dict['ko'][char]}.compact.join('')
 
-        {first_name: first_name.titleize, last_name: last_name.titleize}
+        {
+          first_name: first_name.titleize,
+          last_name: last_name.titleize
+        }
       else
-        {first_name: @en_first_name, last_name: @en_last_name}
+        {
+          first_name: @en_first_name,
+          last_name: @en_last_name
+        }
       end
     end
 
